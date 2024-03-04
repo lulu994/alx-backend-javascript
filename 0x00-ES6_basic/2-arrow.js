8d74637250f810d44e6238cab16bb7c9e987919e
lulu994 <loubna.neddah@gmail.com>
2024-03-04 11:07:17 +0100

    Arrow functions

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,9 @@
-const getNeighborhoodsList = () => {
+export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
+  const self = this;
   this.addNeighborhood = (newNeighborhood) => {
-    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   };
 }
-
-export default getNeighborhoodsList;
